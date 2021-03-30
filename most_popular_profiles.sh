e4410a1af7f568f9b5c8614c1702961c741de11e
edmond120 <edmondwongny@gmail.com>
2021-03-30 14:21:12 -0400

    Add scripts to find the most profiles

---
+++ most_popular_profiles.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/bash
+
+export LC_ALL=C
+
+find_popular_profiles() {(
+	dataset_dir=$1
+	profile=$2
+	echo "find_popular_profiles $dataset_dir $profile"
+	cd "$dataset_dir"
+	mkdir -p "most_popular_profiles_table/$profile/"
+	while read graph; do
+		echo -e "\t$graph"
+		lines=$(wc -l "graphs/$graph" | cut -d ' ' -f 1)
+		./programs/profiler/profiler neighborhood "$profile" "graphs/$graph" \
+		| pv --buffer-size 512M --line-mode --size "$lines" | sort | uniq -c | sort -n -r \
+		> "most_popular_profiles_table/$profile/${graph/%\.g6}"
+	done <<< "$(ls graphs)"
+)}
+
+if [ $# == 0 ]; then
+	echo "args: [dir in datasets...]"
+	exit
+fi
+
+for dataset_dir in "$@"; do
+	find_popular_profiles "$dataset_dir" Imax
+	find_popular_profiles "$dataset_dir" Imin
+	find_popular_profiles "$dataset_dir" Emax
+	find_popular_profiles "$dataset_dir" Emin
+	find_popular_profiles "$dataset_dir" Range
+	find_popular_profiles "$dataset_dir" Id
+	find_popular_profiles "$dataset_dir" Sum
+done
